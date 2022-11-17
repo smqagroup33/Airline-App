e73fd7004676de0659ab59eb12b21c884fd9ba20
shoaib <msr33@student.le.ac.uk>
2022-11-17 14:34:53 +0000

    Person details class with new attributes

--- AirlineReservationSystem/src/AirlineReservationSystem/PersonDetails.java
+++ AirlineReservationSystem/src/AirlineReservationSystem/PersonDetails.java
@@ -4,7 +4,6 @@ package AirlineReservationSystem;
 //Boundary value analysis - can be with the person name
 //Test case for Name length 0 ,1,2   and last three values of maximum 
 //Equivalance practioning - Age >11  <18  15
-
 //
 
 public class PersonDetails
