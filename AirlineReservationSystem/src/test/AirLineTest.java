0fa7cb1518fbb560e85fe6c2c1e375e3ffab0bd3
shoaibreddypalli <115700194+shoaibreddypalli@users.noreply.github.com>
2022-11-17 12:14:27 +0000

    Add files via upload

---
+++ AirlineReservationSystem/src/test/AirLineTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AirLineTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
