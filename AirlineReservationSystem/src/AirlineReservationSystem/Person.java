589c8c2c643cc8b6109380132d6d234f94e83906
dc <dileep.valluru@example.com>
2022-11-19 12:43:40 +0000

    Passenger details format

--- AirlineReservationSystem/src/AirlineReservationSystem/Person.java
+++ AirlineReservationSystem/src/AirlineReservationSystem/Person.java
@@ -11,16 +11,16 @@ public class Person {
 	public static void showtable() {
 		int cntr = 0;
 		int i = 0;
-		int TotalCol = 55;
+		int TotalCol = 100;
 		while (i < TotalCol) {
 			System.out.print("-");
 			i++;
 		}
 		System.out.println();
-		System.out.printf("%5s | %-5s | %-5s | %-10s | %-10s |\n", "Index", "Full Name", "Address", "Age", "Ph Num");
+		System.out.printf("%5s | %-20s | %-30s | %-10s | %-10s |\n", "Index", "Full Name", "Address", "Age", "Ph Num");
 		i = 0;
 		while (i < TotalCol) {
-			if (i == 6 || i == 18 || i == 28 || i == 41)
+			if (i == 6 || i == 29 || i == 62 || i == 75)
 				System.out.print("|");
 			else
 				System.out.print("-");
@@ -31,7 +31,7 @@ public class Person {
 			System.out.println("\t==> No Customers added yet <==");
 		}
 		for (Person pObject : ProjectDB.person_list) {
-			System.out.printf("%5d | %-9s | %-7s | %-10s | %-10s |\n", ++cntr, pObject.PersnDet.sName,
+			System.out.printf("%5d | %-20s | %-30s | %-10s | %-10s |\n", ++cntr, pObject.PersnDet.sName,
 					pObject.PersnDet.sAddress, pObject.PersnDet.nAge, pObject.PersnDet.nPhoneNumber);
 		}
 		i = 0;
