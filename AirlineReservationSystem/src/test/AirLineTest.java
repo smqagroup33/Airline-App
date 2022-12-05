b0ad71258edafbc9262c030919503e7a6e91ebf6
shoaib <msr33@student.le.ac.uk>
2022-12-05 13:58:19 +0000

    Updating Junit test cases covering whitebox testing

--- AirlineReservationSystem/src/test/AirLineTest.java
+++ AirlineReservationSystem/src/test/AirLineTest.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 import org.junit.jupiter.api.Test;
-
 import AirlineReservationSystem.flightDetails;
 import AirlineReservationSystem.PassengerDetails;
 import AirlineReservationSystem.Person;
@@ -15,10 +14,9 @@ import AirlineReservationSystem.ProjectDB;
 import AirlineReservationSystem.AirLineApp;
 import AirlineReservationSystem.FlightScheduler;
 
-class AirLineTest {
+class AirLineTest {  
 
 	@Test
-	static
 	void validateAddPersonDetails() {
 		int count = ProjectDB.personlst.size();
 		PersonDetails new_customer = new PersonDetails();
@@ -90,7 +88,7 @@ class AirLineTest {
 		ProjectDB.insert(pass);
 		assertEquals(1, PassengerDetails.getSCFlightPassengersCount(87655));
 	}
- 
+
 	@Test
 	void validateShowOnlyFlightNo() {
 		PersonDetails new_customer = new PersonDetails();
@@ -117,7 +115,7 @@ class AirLineTest {
 		AirLineApp.input = new Scanner("5\n3\n");
 		AirLineApp.printMenuUI();
 	}
- 
+
 	@Test
 	void validateMain_menu1() {
 
@@ -133,15 +131,126 @@ class AirLineTest {
 	}
 
 	@Test
-	void validateMain_menu3() {
+	void validateMain_menu3() { 
 
 		AirLineApp.input = new Scanner("4\n3\n");
 		AirLineApp.printMenuUI();
 	}
-	
+
 	@Test
 	void validateMain_menu_case1() {
 		AirLineApp.input = new Scanner("1\n1\nShoaib\nLE16RT\n28\n7654567\n7\n3\n");
 		AirLineApp.printMenuUI();
+		
+	} 
+
+	@Test 
+	void validateMain_menu_case2() {
+		AirLineApp.input = new Scanner("1\n2\n7\n3\n");
+		AirLineApp.printMenuUI();
+		AirLineApp.input = new Scanner(System.in);
 	}
+
+	@Test
+	void validateMain_menu_case3() {
+		ProjectDB.personlst= new ArrayList<>();
+		
+		AirLineApp.input = new Scanner("1\n3\n7\n3\n");
+		AirLineApp.printMenuUI();
+		
+		AirLineApp.input = new Scanner("1\n1\nShoaib\nLE16RT\n28\n7654567\n7\n3\n");
+		AirLineApp.printMenuUI();
+		AirLineApp.input = new Scanner("1\n3\n1\n7\n3\n");
+		AirLineApp.printMenuUI(); 
+	}
+	 
+	@Test
+	void validateMain_menu_case4() {
+		ProjectDB.personlst= new ArrayList<>();
+		
+		AirLineApp.input = new Scanner("1\n4\n7\n3\n");
+		AirLineApp.printMenuUI();
+		
+		AirLineApp.input = new Scanner("1\n1\nShoaib\nLE16RT\n28\n7654567\n7\n3\n");
+		AirLineApp.printMenuUI();
+		ProjectDB.flightSchedulerlst = new ArrayList<>();
+		AirLineApp.input = new Scanner("1\n4\n1\n7\n3\n");
+		AirLineApp.printMenuUI(); 
+		 
+		AirLineApp.input = new Scanner("2\n1\nLondon\nIndia\n12:00\n1:30\n5\n8\n3\n");
+		AirLineApp.printMenuUI();
+		
+		AirLineApp.input = new Scanner("2\n4\n1\n8\n3\n");
+		AirLineApp.printMenuUI(); 
+		AirLineApp.input = new Scanner("1\n4\n1\n1\n7\n3\n");
+		AirLineApp.printMenuUI();
+		AirLineApp.input = new Scanner(System.in);
+	}
+
+	@Test
+	void validateMain_menu_case5() {
+		AirLineApp.input = new Scanner("1\n5\n7\n3\n");
+		AirLineApp.printMenuUI();
+	}
+
+	@Test
+	void validateMain_menu_case6() {
+		ProjectDB.passengerlst =new ArrayList<>(); 
+		AirLineApp.input = new Scanner("1\n6\n7\n3\n");
+		AirLineApp.printMenuUI();
+		
+		AirLineApp.input = new Scanner("1\n1\nShoaib\nLE16RT\n28\n7654567\n7\n3\n");
+		AirLineApp.printMenuUI();
+		//AirLineApp.input = new Scanner("1\n6\n1\n7\n3\n");
+		//AirLineApp.printMenuUI();
+	}
+
+	@Test
+	void validateMain_menu_case8() {
+		AirLineApp.input = new Scanner("1\n8\n7\n3\n");
+		AirLineApp.printMenuUI();
+	}
+
+	@Test
+	void validateMain_menu_caseInvlaid() {
+		AirLineApp.input = new Scanner("1\n10\n7\n3\n");
+		AirLineApp.printMenuUI();
+		AirLineApp.input = new Scanner(System.in);
+	}
+
+	@Test
+	void validateFlight_menu_case1() {
+		AirLineApp.input = new Scanner("2\n1\nLondon\nIndia\n12:00\n1:30\n5\n8\n3\n");
+		AirLineApp.printMenuUI();
+		AirLineApp.input = new Scanner(System.in);
+	}
+
+	@Test
+	void validateFlight_menu_case2() {
+		AirLineApp.input = new Scanner("2\n2\n8\n3\n");
+		AirLineApp.printMenuUI();
+		AirLineApp.input = new Scanner(System.in);
+	}
+
+	@Test
+	void validateFlight_menu_case5() {
+		AirLineApp.input = new Scanner("2\n5\n8\n3\n");
+		AirLineApp.printMenuUI();
+		AirLineApp.input = new Scanner(System.in);
+	}
+
+	@Test
+	void validateFlight_menu_case9() {
+		AirLineApp.input = new Scanner("2\n9\n8\n3\n");
+		AirLineApp.printMenuUI();
+		AirLineApp.input = new Scanner(System.in);
+	}
+
+	@Test
+	void validateFlight_menu_caseInvlaid() {
+		AirLineApp.input = new Scanner("2\n10\n8\n3\n");
+		AirLineApp.printMenuUI();
+		AirLineApp.input = new Scanner(System.in);
+	}
+
 }
